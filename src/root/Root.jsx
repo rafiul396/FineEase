d1b8ed62efc82a90b50de95c6ab75f4c5ddac73f
rafiul from office <rafiulofficial396@gmail.com>
2025-11-13 21:38:37 +0600

    Implement footer section

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -3,12 +3,14 @@ import Header from '../components/header/Header';
 import { Outlet } from 'react-router';
 import { ToastContainer } from 'react-toastify';
 import { Toaster } from 'react-hot-toast';
+import Footer from '../components/footer/Footer';
 
 const Root = () => {
     return (
         <div className='overflow-x-hidden'>
             <Header />
             <Outlet />
+            <Footer />
             <ToastContainer />
             <Toaster />
         </div>
