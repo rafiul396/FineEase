b08f9f08de740a58096a6508f37e5ab1c77035de
Md. Rafiul Islam <rafiul24.bd@gmail.com>
2025-11-14 10:10:54 +0600

    updated profile menu section

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
     const { user, logOut } = use(AuthContext);
 
     const toggleMenu = () => setIsOpen(!isOpen);
-    const handleFalse = () => setIsOpen(false);
 
     const profileClicker = () => {
         setOpen(!open)
@@ -37,63 +36,57 @@ const Navbar = () => {
                     <li onClick={() => setOpen(false)}><NavLink className="cursor-pointer hover:text-primary hover:border-2 hover:border-primary border-2 border-base-100 px-2 py-1 rounded-full transition-all duration-300" to="/my-transaction">My Transactions</NavLink></li>
                     <li onClick={() => setOpen(false)}><NavLink className="cursor-pointer hover:text-primary hover:border-2 hover:border-primary border-2 border-base-100 px-2 py-1 rounded-full transition-all duration-300" to="/report">Reports</NavLink></li>
                 </ul>
-                <div className="hidden lg:flex items-center space-x-4">
-                    {
-                        user ? (
-                            <>
-                                <Switch />
-                                <img onClick={profileClicker} title={user?.displayName} className='w-12 h-12 cursor-pointer object-fill rounded-full border-2 border-primary p-[2px]' src={user.photoURL ? user.photoURL : userLogo} alt="Profile Picture" />
-                                {open && (
-                                    <div className="absolute right-16 top-14 w-48 bg-accent-content border border-accent rounded shadow-lg z-50 space-y-2 py-2">
-                                        <div className='p-2 text-secondary'>
-                                            <p className="font-semibold text-base">{user.displayName}</p>
-                                            <p className=" text-sm">{user.email}</p>
-                                        </div>
-                                        <Link to="/my-profile" onClick={() => setOpen(false)} className=" border border-accent w-full flex justify-center items-center text-secondary text-left px-4 py-2"> <FiUser className="text-lg text-primary" /> My Profile</Link>
-                                        <button className='bg-orange-500 w-full cursor-pointer text-white text-sm px-5 py-2 rounded-full hover:bg-primary transition' onClick={logOut}>
-                                            Log out
-                                        </button>
+
+                {/* Mobile Menu */}
+                <div className='flex gap-2'>
+                    <div className="lg:hidden flex items-center">
+                        <button
+                            onClick={toggleMenu}
+                            className="text-secondary focus:outline-none"
+                        >
+                            {isOpen ? <HiX size={26} /> : <HiMenuAlt3 size={26} />}
+                        </button>
+                    </div>
+
+                    <div className="flex items-center space-x-4">
+                        {
+                            user ? (
+                                <>
+                                    <div onClick={() => setIsOpen(false)} className="drop-menu dropdown dropdown-end">
+                                        <img tabIndex={0} role="button" onClick={profileClicker} title={user?.displayName} className='w-12 h-12 cursor-pointer object-fill rounded-full border-2 border-primary p-[2px]' src={user.photoURL ? user.photoURL : userLogo} alt="Profile Picture" />
+                                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm space-y-2">
+                                            <div className='p-2 text-secondary'>
+                                                <p className="font-semibold text-base">{user.displayName}</p>
+                                                <p className=" text-sm">{user.email}</p>
+                                            </div>
+                                            <NavLink to="/my-profile" onClick={() => setOpen(false)} className=" border border-accent w-full flex justify-center items-center text-secondary text-left px-4 py-2 space-x-2"> <FiUser className="text-lg text-primary" /> <span>My Profile</span></NavLink>
+                                            <Switch />
+                                            <button className='bg-orange-500 w-full cursor-pointer text-white text-sm px-5 py-2 rounded-full hover:bg-primary transition' onClick={logOut}>
+                                                Log out
+                                            </button>
+                                        </ul>
                                     </div>
-                                )}
 
-                            </>
-                        ) : (
-                            <>
+                                </>
+                            ) : (
+                                <>
 
-                                <Switch />
-                                <Link to="/login" className="bg-[#0d1b2a] cursor-pointer text-white text-sm px-5 py-2 rounded-full hover:opacity-90 transition">
-                                    Log In
-                                </Link>
-                                <Link to="/signup" className="bg-orange-500 cursor-pointer text-white text-sm px-5 py-2 rounded-full hover:bg-primary transition">
-                                    Sign Up
-                                </Link>
-                            </>
-                        )
-                    }
-                </div>
-                <div className="lg:hidden flex items-center">
-                    <button
-                        onClick={toggleMenu}
-                        className="text-gray-700 focus:outline-none"
-                    >
-                        {isOpen ? <HiX size={26} /> : <HiMenuAlt3 size={26} />}
-                    </button>
-                    {
-                        user &&
-                        <img title={user?.displayName} className='w-12 h-12 ml-4 cursor-pointer object-fill rounded-full border-2 border-secondary p-1' src={user.photoURL ? user.photoURL : userLogo} alt="" />
-                    }
+                                    <Switch />
+                                    <Link to="/login" className="bg-[#0d1b2a] cursor-pointer text-white text-sm px-5 py-2 rounded-full hover:opacity-90 transition">
+                                        Log In
+                                    </Link>
+                                    <Link to="/signup" className="bg-orange-500 cursor-pointer text-white text-sm px-5 py-2 rounded-full hover:bg-primary transition">
+                                        Sign Up
+                                    </Link>
+                                </>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
             {isOpen && (
                 <div className="p-4 mt-4 w-full bg-base-100 lg:hidden flex flex-col space-y-3 text-gray-700 font-medium absolute z-50 top-12 left-0">
                     <ul className="header-menu flex flex-col space-y-3">
-                        {/* <div className="absolute right-16 top-14 w-48 bg-accent-content border border-accent rounded shadow-lg z-50 space-y-2 py-2"> */}
-                            <div className='p-2 text-secondary'>
-                                <p className="font-semibold text-base">{user.displayName}</p>
-                                <p className=" text-sm">{user.email}</p>
-                            </div>
-                            <Link to="/my-profile" onClick={toggleMenu} className=" border border-accent w-full flex justify-center items-center text-secondary text-left px-4 py-2"> <FiUser className="text-lg text-primary" /> My Profile</Link>
-                        {/* </div> */}
                         <li><NavLink className="cursor-pointer hover:text-primary hover:border-2 hover:border-primary border-2 border-base-100 px-2 py-1 rounded-full transition-all duration-300" to="/" onClick={toggleMenu} >Home</NavLink></li>
                         <li><NavLink className="cursor-pointer hover:text-primary hover:border-2 hover:border-primary border-2 border-base-100 px-2 py-1 rounded-full transition-all duration-300" to="/add-transaction" onClick={toggleMenu}>Add Transaction</NavLink></li>
                         <li><NavLink className="cursor-pointer hover:text-primary hover:border-2 hover:border-primary border-2 border-base-100 px-2 py-1 rounded-full transition-all duration-300" to="/my-transaction" onClick={toggleMenu}>My Transactions</NavLink></li>
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,22 +5,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey, 
-//   authDomain: import.meta.env.VITE_authDomain, 
-//   projectId: import.meta.env.VITE_projectId, 
-//   storageBucket: import.meta.env.VITE_storageBucket, 
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId, 
-//   appId: import.meta.env.VITE_appId, 
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCmGDs5obNzs2rRGfTll9bwZmAG4O8USSQ",
-  authDomain: "fineease-fe3d1.firebaseapp.com",
-  projectId: "fineease-fe3d1",
-  storageBucket: "fineease-fe3d1.firebasestorage.app",
-  messagingSenderId: "299611020521",
-  appId: "1:299611020521:web:e0e41f7b5f2bcf139c2478"
+  apiKey: import.meta.env.VITE_apiKey, 
+  authDomain: import.meta.env.VITE_authDomain, 
+  projectId: import.meta.env.VITE_projectId, 
+  storageBucket: import.meta.env.VITE_storageBucket, 
+  messagingSenderId: import.meta.env.VITE_messagingSenderId, 
+  appId: import.meta.env.VITE_appId, 
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCmGDs5obNzs2rRGfTll9bwZmAG4O8USSQ",
+//   authDomain: "fineease-fe3d1.firebaseapp.com",
+//   projectId: "fineease-fe3d1",
+//   storageBucket: "fineease-fe3d1.firebasestorage.app",
+//   messagingSenderId: "299611020521",
+//   appId: "1:299611020521:web:e0e41f7b5f2bcf139c2478"
+// };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
